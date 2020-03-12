c5ea054191d917f99d888e2c12cf2deb56862843
Amber Mohler <ambermohler@iMac.local>
2020-03-12 12:48:49 -0400

    added scripts start to package.json

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require('./api/server.js');
 
-const port = 4000;
+
+// want port to be assigned by the Server 
+//  Heroku will place the .PORT environment value on their server
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
